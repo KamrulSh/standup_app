b4aad20317c63c7536dffe98cf182f79a11ee977
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-05-24 21:34:04 +0600

    Add rspec testing

--- spec/views/activity/feed.html.tailwindcss_spec.rb
+++ spec/views/activity/feed.html.tailwindcss_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "activity/feed.html.tailwindcss", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "activity/feed.html.erb", type: :view do
+  it "renders the word feed" do
+    render template: 'activity/feed'
+    expect(rendered).to match(/feed/)
+  end
 end
